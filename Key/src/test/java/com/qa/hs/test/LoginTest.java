737f26afc0bc97711d9a70e721370394f165cd24
karth <karth@DESKTOP-99QFCAM>
2022-05-16 09:40:35 +0530

    First Commit
    Uploading All files

---
+++ Key/src/test/java/com/qa/hs/test/LoginTest.java
@@ -0,0 +1,17 @@
+package com.qa.hs.test;
+
+import org.testng.annotations.Test;
+
+import com.qa.hs.keyword.engine.KeywordEngine;
+
+public class LoginTest {
+
+	public KeywordEngine keywordEngine;	
+	
+	@Test
+	public void loginTest() {
+		keywordEngine = new KeywordEngine();
+		keywordEngine.startExecution("login");
+	}
+	
+}
